b1cf815540bd6e1a104ce1e0cd1edd5839846ac0
AmalHanif <amalhanif04@gmail.com>
2018-05-08 22:37:05 -0700

    Update OAuth2

--- src/components/authorizations/OAuth2.js
+++ src/components/authorizations/OAuth2.js
@@ -1,22 +1,129 @@
 import React, { Component } from "react";
 import Dialog from 'react-dialog';
 import {Form,Col,FormGroup,ControlLabel,FormControl, Button } from 'react-bootstrap';
+import update from 'immutability-helper';
+
+function FieldGroup({ id, label, help, ...props }) {
+  return (
+    <FormGroup controlId={id}>
+      <Col componentClass={ControlLabel} md={4}>{label}</Col>
+      <Col md={8}>
+        <FormControl {...props} />
+      
+      </Col>
+  </FormGroup>
+  );
+}
 
 class OAuth2 extends Component {
-  constructor(props) {
+  constructor(props, context) {
     super(props);
+     this.handleChange = this.handleChange.bind(this); 
     this.state = {
-      isDialogOpen:false,   
-    }
+      isDialogOpen:false, 
+      value:'',
+      initialUrlForPopup:'',
+      popupUrl:'',
+      access_token:'',
+      requestToken:[
+        {
+          id:0,
+          name:"TokenName",
+          value:""
+        },
+        {
+          id:1,
+          name:"GrantType",
+          value:""
+        },
+        {
+          id:2,
+          name:"CallBackURL",
+          value:""
+        },
+        {
+          id:3,
+          name:"AuthURL",
+          value:""
+        },
+        {
+          id:4,
+          name:"AccessTokenURL",
+          value:""
+        },
+        {
+          id:5,
+          name:"ClientID",
+          value:""
+        },
+        {
+          id:6,
+          name:"ClientSecret",
+          value:""
+        },
+        {
+          id:7,
+          name:"Scope",
+          value:""
+        },
+        {
+          id:8,
+          name:"State",
+          value:""
+        },
+        {
+          id:9,
+          name:"ClientAuthentication",
+          value:""
+        }   
+      ],
+    };
+    this.openChildwindow=this.openChildwindow.bind(this);  
+ 
   }
   openDialog = () => this.setState({ 
     isDialogOpen: true
   })
 
-  handleClose = () => this.setState({
-      isDialogOpen: false
+  handleClose = () =>this.setState({
+      isDialogOpen: false 
   })
-      
+  
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+
+    let obj=[],requestobj=[],updated=[];
+    obj.name= event.target.name;
+    obj.value=event.target.value;
+    requestobj=this.state.requestToken
+    requestobj.forEach(function(element) {
+      if(obj.name===element.name ){
+        updated= update(requestobj[element.id],{$merge:{name:obj.name, value: obj.value}})
+        requestobj[element.id]=updated;
+      }
+    });
+    this.state.requestToken=requestobj;
+    // this.setState( {requestToken: requestobj});
+    console.log(this.state.requestToken)
+  }
+
+  openChildwindow(){
+    // let request= this.state.requestToken;
+    // const initialUrlForPopup = window.open(request[3].value+"?response_type=code&client_id="+request[5].value+"&client_secret="+request[6].value+"&grant_type=authorization_code&code=AUTHORIZATION_CODE&redirect_uri="+request[2].value);
+    const initialUrlForPopup = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=81khbl128ifn8q&client_secret=JTtqvuUBN9bVTlG4&grant_type=authorization_code&redirect_uri=http://localhost:3000/oauth/callback";
+    this.state.popupUrl= window.open( initialUrlForPopup, "" ,"height=500,width=500",);
+    var popup= this.state.popupUrl;
+    const intervalID = setTimeout(checkUrl, 10000)
+    let prevLocation = initialUrlForPopup
+    function checkUrl() {
+      if ( popup.location !== prevLocation) {
+        console.log(popup.location.search)
+        prevLocation = popup.location.href;
+      }
+    }
+    return( this.state.popupUrl.location)
+  }
+
 
   render() {
     return (
@@ -33,70 +140,39 @@ class OAuth2 extends Component {
             </FormGroup> 
             <Button bsStyle="warning" type="button" onClick={this.openDialog}>Get New Access Token</Button>
             {this.state.isDialogOpen &&
-              <Dialog class="dialog" title="Dialog Title" modal={true} onClose={this.handleClose} buttons={[{ text: "Close", onClick: () => this.handleClose()}]}>             
-                <Form>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>Token Name </Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="Token Name" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>Grant Type </Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="Authorization Code" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>Callback URL </Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="http://your-application.com/registered/callback" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>Auth URL </Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="https://example.com/login/oauth/authorize" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>Access Token URL </Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="https://example.com/login/oauth/access_token" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>Client ID </Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="Client ID" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>Client Secret</Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="Client Secret" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>Scope </Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="e.g. read:org" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>State  </Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="State" />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} md={6}>Client Authentication  </Col>
-                    <Col md={6}>
-                      <FormControl type="text" placeholder="" />
-                    </Col>
-                  </FormGroup>      
-                </Form>
-              </Dialog>
+            <Dialog class="dialog" title="Dialog Title" modal={true} onClose={this.handleClose} >             
+              <Form>
+                <FieldGroup name="TokenName" label="Token Name" onBlur={this.handleChange} type="text" value={this.state.token} data-token="" placeholder="Token Name"  />
+              <FormGroup controlId="formGrantType">
+                <Col componentClass={ControlLabel} md={4}>Grant Type </Col>
+                <Col md={8}>
+                  <FormControl name="GrantType" onBlur={this.handleChange} componentClass="select" value={this.state.GrantType} placeholder="select" >
+                    <option value="AuthorizationCode" >Authorization Code</option>
+                    <option value="Implicit" >Implicit </option>
+                    <option value="PasswordCardiential">Password Cardiential</option>
+                    <option value="ClientCardiential">Client Cardiential</option>
+                  </FormControl>
+                </Col>
+              </FormGroup> 
+              <FieldGroup name="CallBackURL" label="Call back URL"  onBlur={this.handleChange} type="url" value={this.state.CallbackURL} placeholder="http://your-application.com/registered/callback" />
+              <FieldGroup name="AuthURL" label="Auth URL"  onBlur={this.handleChange} type="url" value={this.state.AuthURL} placeholder="https://example.com/login/oauth/authorize" />
+              <FieldGroup name="AccessTokenURL" label="Access Token URL"  onBlur={this.handleChange} type="url" value={this.state.AccessTokenURL} placeholder="https://example.com/login/oauth/access_token" />
+              <FieldGroup name="ClientID" label="ClientID"  onBlur={this.handleChange} type="text" value={this.state.ClientID} placeholder="Client ID" />
+              <FieldGroup name="ClientSecret" label="ClientSecret"  onBlur={this.handleChange} type="text" value={this.state.ClientSecret} placeholder="Client Secret" />
+              <FieldGroup name="Scope" label="Scope"  onBlur={this.handleChange} type="text" value={this.state.Scope} placeholder="e.g. read:org" />
+              <FieldGroup name="State" label="State" onBlur={this.handleChange} type="text" value={this.state.formState} placeholder="State" />
+              <FormGroup controlId="formClientAuthentication">
+                <Col componentClass={ControlLabel} md={4}>Client Authentications </Col>
+                <Col md={8}>
+                  <FormControl name="ClientAuthentication" onBlur={this.handleChange} componentClass="select"  value={this.state.ClientAuthentication} placeholder="select" >
+                    <option value="BasicAuthHeader" >Send as Basic Auth header</option>
+                    <option value="clientCardientialInBody" >Send as client cardiential in body </option>
+                  </FormControl>
+                </Col>
+              </FormGroup>    
+              <Button onClick={this.openChildwindow} bsStyle="warning" type="button">Request Token</Button>
+              </Form>
+            </Dialog>
             }
           </Col>
         </FormGroup>
@@ -104,5 +180,6 @@ class OAuth2 extends Component {
     );
   }
 }
+
 
 export default OAuth2;
--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ import Authorization from "./components/authorizations/authorization";
 import Header from "./components/Header";
 import Body from "./components/body/body";
 import Test from "./components/test";
-
+import Testing from "./components/testing";
 
 class Main extends Component {
   constructor(props, context) {
-    super(props, context);
+    super();
   
     this._columns = [
       {
@@ -43,7 +43,7 @@ class Main extends Component {
        value:"",
        query:"?key=12234",
        finalQuery : "",
-
+       currentRowId:0,
        rows: this.createRows(1),
        showMe:false,
        isDialogOpen: false
@@ -81,61 +81,7 @@ class Main extends Component {
 
     return clonedColumns;
   };
-
-
-  handleGridRowsUpdated = ({ fromRow, toRow, rowIds, updated,newRowIndex,numberOfRows }) => {
-    let rows = this.state.rows.slice();
-    console.log(fromRow,toRow, rowIds)
-    for (let i = fromRow; i <= toRow; i++) {
-      if(this.state.rows.length===rows[i].id){ //checked Is this last row?
-        let rowToUpdate = rows[i];
-        let updatedRow = update(rowToUpdate, {$merge: updated});
-        rows[i] = updatedRow;
-        if(rows[i].key!=="" || rows[i].value!=="" ||rows[i].description!=="" ){ //If any cell in the last row is not empty then creat new row.
-          const newRow = {
-            id: rows[i].id+1 ,
-            key:'',
-            value: '',
-            description: '',
-          };
-          rows = update(rows, {$push: [newRow]});   
-        }
-      }else{
-        let rowToUpdate = rows[i];
-        let updatedRow = update(rowToUpdate, {$merge: updated});
-        rows[i] = updatedRow;
-      } 
-    }  
-      // if(rows[i].id===1){
-      //   this.setState({
-      //     Qkey:"?"+rows[i].key ,
-      //     Qvalue:"="+rows[i].value},function(){
-      //       this.setState({
-      //         query:this.state.Qkey + this.state.Qvalue
-      //       })  
-      //     }
-      //   )
-      // }else{
-      //   this.setState({
-      //     Qkey:"&"+rows[i].key ,
-      //     Qvalue:"="+rows[i].value},function(){
-      //       this.setState((prevState) => {
-      //         return {query:prevState.query+this.state.Qkey + this.state.Qvalue} 
-      //       })  
-      //     }
-      //   )
-      // }
-      
   
-    
-    // for(let j=0; j<=2; j++ ){
-    //   console.log(this.state.rows[j].key);
-    //   query="?"+ this.state.rows[j].key+"="+this.state.rows[j].value;
-    //   sum= sum+query;
-    // }
-    // this.setState({ rows })
-  };
-
   getRowAt = (index) => {
     if (index < 0 || index > this.getSize()) {
       return undefined;
@@ -154,44 +100,34 @@ class Main extends Component {
     })
   };
   
-  handleChange( updated){
-      // console.log(this.state)
-      console.log(updated);
-      if (updated.cellKey === "key" ) {
-       this.state.rows[updated.fromRow].key =  updated.updated.key;
-      }else if(updated.cellKey === "value"){
-        this.state.rows[updated.fromRow].value =  updated.updated.value;
-      }
-      // only add if row has both key and value
-      let currentRow = this.state.rows[updated.fromRow];
+  handleChange(event){
+    let rowToUpdate = this.state.rows[event.fromRow]
+    let updatedRow = update(rowToUpdate, {$merge: event.updated});
+    this.state.rows[event.fromRow]= updatedRow;
+    console.log(event);
 
-      if (currentRow.key && currentRow.value) {
-        this.state.rows[updated.fromRow].is_complete = true
+    // only add if row has both key and value
+    let currentRow = this.state.rows[event.fromRow];
+
+    if (currentRow.key && currentRow.value) {
+      this.state.rows[event.fromRow].is_complete = true
+    }
+    // add new row
+    if(this.state.rows.length===event.fromRowId){
+      if(currentRow.key!=="" || currentRow.value!=="" ||currentRow.description!=="" ){ 
+        const newRow = this.createRowObjectData(currentRow.id);
+        this.state.rows = update( this.state.rows, {$push: [newRow]});   
       }
-      // add new row
-      if(this.state.rows.length===updated.fromRowId){
-        if(currentRow.key!=="" || currentRow.value!=="" ||currentRow.description!=="" ){ //If any cell in the last row is not empty then creat new row.
-          const newRow = this.createRowObjectData(currentRow.id);
-          this.state.rows = update( this.state.rows, {$push: [newRow]});   
-        }
-      }  
-      // ----
-  
-      this.triggerQueryChange();
-       
-      console.log(this.state);
-      // this.setState({url:event.target.value},function () {
-      //   this.setState({
-      //     value:this.state.url+this.state.query },function () {
-      //     console.log(this.state)
-      //     })
-      // })
-    
+    }  
+    // ----
+
+    this.triggerQueryChange(currentRow.id-1);
+      
+    console.log(this.state);
   };
 
-  triggerQueryChange(){
-
-    let query = this.parseQuery(this.state.url);
+  triggerQueryChange(currentRowId){
+    let query = this.parseQuery(this.state.url,currentRowId);
     var finalQueryBuilder = "";
     finalQueryBuilder += query.urlValue;
 
@@ -208,18 +144,17 @@ class Main extends Component {
       }
     }
     this.setState({url :finalQueryBuilder});
-    
   }
+
   handleUrl(updated){
     console.log(updated.target.value)
-    // this.setState({url :""});
-    
-    this.setState({url : updated.target.value},function(){this.parseQuery(this.state.url);});
-    // this.state.url = updated.target.value;
-    
-
-    
+    this.setState({url : updated.target.value},
+      function(){
+        this.parseQuery(this.state.url);
+      }
+    );
   }
+  
   findInArray(array, needle){
     for (var i = 0; i < array.length; i++) {
       var element = array[i];
@@ -229,7 +164,8 @@ class Main extends Component {
     }
     return -1;
   }
-  parseQuery(url){
+
+  parseQuery(url,currentRowId){
     let obj = {};
     obj.params = {};
     let questionMark = url.indexOf('?');
@@ -239,104 +175,116 @@ class Main extends Component {
       obj.urlValue = url.substring(0,questionMark);
     }
     var regex = /[?&]([^=#]+)=([^&#]*)/g,
-     match;
-    // let rowHolder = Object.assign({}, this.state.rows);
-    // let rowHolder = this.state.rows;
+        match;
+
     let rowHolder = this.state.rows;
-    this.setState( {rows: null});
-    console.log(this.state.rows)
-    while (match = regex.exec(url)) {
-      obj.params[match[1]] = match[2];
-      // replace if exists
-      let index = this.findInArray  (rowHolder, match[1]);
-      if (index > -1) {
-        rowHolder[index].value = match[2];
-      }
-      rowHolder.push({key:match[1], value: match[2]});
+    let updatedRow=[];
+
+    if(currentRowId===undefined){
+      this.setState({rows:null});
+      let count=0;
+      while (match = regex.exec(url)) {
+        obj.params[match[1]] = match[2];
+        // replace if exists
+        let index = this.findInArray  (rowHolder, match[1]);
+        if (index > -1) {
+          rowHolder[index].value = match[2];
+          updatedRow= update(rowHolder[index],{$merge:{key:match[1], value: match[2]}})
+          rowHolder[count]=updatedRow;
+        }else{
+          if(rowHolder[count]===rowHolder.lenght){ 
+            rowHolder.push({key:match[1], value: match[2]})
+            const newRow = this.createRowObjectData(count);
+            rowHolder = update( this.state.rows, {$push: [newRow]});
+          }
+          else{
+            updatedRow= update(rowHolder[count],{$merge:{key:match[1], value: match[2]}})
+            rowHolder[count]=updatedRow;
+          }
+        }
+        count=+1;
+      }  
     }
-    
     this.setState( {rows: rowHolder});
 
-    
     console.log(this.state);
     return obj;
   }
-    render() {
-      return (
-        // console.log(this.state.rows),
-        <div >
-          <Row >
-          <Col componentClass={FormGroup} md={10}>
-            <InputGroup>
-              <DropdownButton componentClass={InputGroup.Button} id="input-dropdown-addon" title="action">
-                <MenuItem value="GET" eventKey="GET" >GET</MenuItem>
-                <MenuItem value="POST" eventKey="POST">POST</MenuItem>              
-                <MenuItem value="PUT" eventKey="PUT">PUT</MenuItem>
-                <MenuItem value="PATCH" eventKey="PATCH">PATCH</MenuItem>
-                <MenuItem value="DELETE" eventKey="DELETE">DELETE</MenuItem>
-                <MenuItem value="COPY" eventKey="COPY">COPY</MenuItem>
-                <MenuItem value="HEAD" eventKey="HEAD">HEAD</MenuItem>              
-                <MenuItem value="OPTIONS" eventKey="OPTIONS">OPTIONS</MenuItem>
-                <MenuItem value="LINK" eventKey="LINK">LINK</MenuItem>
-                <MenuItem value="UNLINK" eventKey="UNLINK">UNLINK</MenuItem>
-                <MenuItem value="PURGE" eventKey="PURGE">PURGE</MenuItem>
-                <MenuItem value="LOCK" eventKey="LOCK">LOCK</MenuItem>
-                <MenuItem value="UNLOCK" eventKey="UNLOCK">UNLOCK</MenuItem>
-                <MenuItem value="PROFIND" eventKey="PROFIND">PROFIND</MenuItem>
-                <MenuItem value="VIEW" eventKey="VIEW">VIEW</MenuItem>
-              </DropdownButton>
-              <FormControl type="text" placeholder="Enter request URL" value={this.state.url} onChange={this.handleUrl}/>
-              {/* <FormControl type="text" placeholder="Enter request URL"  value={this.state.url} />  */}
-              <InputGroup.Button>
-                <Button  onClick={() => this.operation()}>Params</Button>
-              </InputGroup.Button>
-            </InputGroup>
-          </Col>
-          <Col >
-            <Row  componentClass={FormGroup}>
-              <SplitButton id="send" bsStyle="info" title="Send">
-                <MenuItem eventKey="1">Send and Download</MenuItem>
-              </SplitButton>
-              <SplitButton id="save" title="Save">
-              < MenuItem eventKey="1">Save As</MenuItem>
-              </SplitButton>
-            </Row>
-          </Col>
-        </Row> 
-       { this.state.showMe?<div><ReactDataGrid 
-          ref={ node => this.grid = node }
-          enableCellSelect={true}
-          columns={this.getColumns()}
-          rowGetter={this.getRowAt}
-          rowsCount={this.getSize()}
-          enableRowSelect={true}
-          onGridRowsUpdated={this.handleChange}
-          rowHeight={40}
-          minHeight={250}
-          minWidth={1080}
-          rowScrollTimeout={200} /></div>
-          :null
-        } 
-        
-        <HashRouter>
-          <div>
-            <ul className="navBar">
-                <li><NavLink exact to="/">Authorization</NavLink></li>
-                <li><NavLink to="/header">Header</NavLink></li>
-                <li><NavLink to="/body">Body</NavLink></li>
-                <li><NavLink to="/test">Test</NavLink></li>
-            </ul>
-            <div className="content">
-                <Route exact path="/"  component={Authorization}/>
-                <Route path="/header" component={Header}/>
-                <Route path="/body" component={Body}/>
-                <Route path="/test" component={Test}/>
-            </div>
+  render() {
+    return (
+      <div >
+        <Row >
+        <Col componentClass={FormGroup} md={10}>
+          <InputGroup>
+            <DropdownButton componentClass={InputGroup.Button} id="input-dropdown-addon" title="action">
+              <MenuItem value="GET" eventKey="GET" >GET</MenuItem>
+              <MenuItem value="POST" eventKey="POST">POST</MenuItem>              
+              <MenuItem value="PUT" eventKey="PUT">PUT</MenuItem>
+              <MenuItem value="PATCH" eventKey="PATCH">PATCH</MenuItem>
+              <MenuItem value="DELETE" eventKey="DELETE">DELETE</MenuItem>
+              <MenuItem value="COPY" eventKey="COPY">COPY</MenuItem>
+              <MenuItem value="HEAD" eventKey="HEAD">HEAD</MenuItem>              
+              <MenuItem value="OPTIONS" eventKey="OPTIONS">OPTIONS</MenuItem>
+              <MenuItem value="LINK" eventKey="LINK">LINK</MenuItem>
+              <MenuItem value="UNLINK" eventKey="UNLINK">UNLINK</MenuItem>
+              <MenuItem value="PURGE" eventKey="PURGE">PURGE</MenuItem>
+              <MenuItem value="LOCK" eventKey="LOCK">LOCK</MenuItem>
+              <MenuItem value="UNLOCK" eventKey="UNLOCK">UNLOCK</MenuItem>
+              <MenuItem value="PROFIND" eventKey="PROFIND">PROFIND</MenuItem>
+              <MenuItem value="VIEW" eventKey="VIEW">VIEW</MenuItem>
+            </DropdownButton>
+            <FormControl type="text" placeholder="Enter request URL" value={this.state.url} onChange={this.handleUrl}/>
+            <InputGroup.Button>
+              <Button  onClick={() => this.operation()}>Params</Button>
+            </InputGroup.Button>
+          </InputGroup>
+        </Col>
+        <Col >
+          <Row  componentClass={FormGroup}>
+            <SplitButton id="send" bsStyle="info" title="Send">
+              <MenuItem eventKey="1">Send and Download</MenuItem>
+            </SplitButton>
+            <SplitButton id="save" title="Save">
+            < MenuItem eventKey="1">Save As</MenuItem>
+            </SplitButton>
+          </Row>
+        </Col>
+      </Row> 
+      { this.state.showMe?<div><ReactDataGrid 
+        ref={ node => this.grid = node }
+        enableCellSelect={true}
+        columns={this.getColumns()}
+        rowGetter={this.getRowAt}
+        rowsCount={this.getSize()}
+        enableRowSelect={true}
+        onGridRowsUpdated={this.handleChange}
+        rowHeight={40}
+        minHeight={150}
+        minWidth={1080}
+        rowScrollTimeout={200} /></div>
+        :null
+      } 
+      
+      <HashRouter>
+        <div>
+          <ul className="navBar">
+              <li><NavLink exact to="/">Authorization</NavLink></li>
+              <li><NavLink to="/header">Header</NavLink></li>
+              <li><NavLink to="/body">Body</NavLink></li>
+              <li><NavLink to="/test">Test</NavLink></li>
+          </ul>
+          <div className="content">
+              <Route exact path="/"  component={Authorization}/>
+              <Route path="/header" component={Header}/>
+              <Route path="/body" component={Body}/>
+              <Route path="/test" component={Test}/>
+              <Route path="/testing" component={Testing}/>
           </div>
-        </HashRouter>
-       </div> 
-      );
-    }
+        </div>
+      </HashRouter>
+      </div> 
+    );
   }
+}
 
 export default Main;
